e1056a5aaab9283bc8e3eff211b3d2bb74d5ee38
Eldar Dragomir <eldar.dragomir@gmail.com>
2024-01-25 21:40:23 +0100

    terraform code

---
+++ 01-docker-terraform/1_terraform_gcp/code/variables.tf
@@ -0,0 +1,34 @@
+variable "credentials" {
+  description = "My Credentials"
+  default     = "./keys/creds.json"
+}
+
+
+variable "project" {
+  description = "Project"
+  type        = string
+}
+
+variable "region" {
+  description = "Region"
+  type        = string
+}
+
+variable "location" {
+  description = "Project Location"
+  type        = string
+}
+
+variable "bq_dataset_name" {
+  description = "My BigQuery Dataset Name"
+  type        = string
+}
+
+variable "gcs_bucket_name" {
+  description = "My Storage Bucket Name"
+}
+
+variable "gcs_storage_class" {
+  description = "Bucket Storage Class"
+  default     = "STANDARD"
+}
