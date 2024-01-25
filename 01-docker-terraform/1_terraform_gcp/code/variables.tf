bc216f19a9285e3b8f4b3882f1b88a7fd7e7da90
Eldar Dragomir <eldar.dragomir@gmail.com>
2024-01-25 21:44:12 +0100

    type string

--- 01-docker-terraform/1_terraform_gcp/code/variables.tf
+++ 01-docker-terraform/1_terraform_gcp/code/variables.tf
@@ -1,6 +1,7 @@
 variable "credentials" {
   description = "My Credentials"
   default     = "./keys/creds.json"
+  type        = string
 }
 
 
@@ -26,9 +27,11 @@ variable "bq_dataset_name" {
 
 variable "gcs_bucket_name" {
   description = "My Storage Bucket Name"
+  type        = string
 }
 
 variable "gcs_storage_class" {
   description = "Bucket Storage Class"
   default     = "STANDARD"
+  type        = string
 }
