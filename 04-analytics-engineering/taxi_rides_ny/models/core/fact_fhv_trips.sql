b5eb2564d8635e3415f01107b07ed7538ae7fd49
Eldar Dragomir <eldar.dragomir@gmail.com>
2024-02-19 15:57:57 +0000

    fhv2

--- 04-analytics-engineering/taxi_rides_ny/models/core/fact_fhv_trips.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/core/fact_fhv_trips.sql
@@ -5,7 +5,7 @@ with
         select *, 'FHV' as service_type from {{ ref("stg_fhv_tripdata") }}
     ),
     dim_zones as (select * from {{ ref("dim_zones") }} where borough != 'Unknown')
-select fhv_tripdata.PUlocationID, fhv_tripdata.DOlocationID from fhv_tripdata
+select * from fhv_tripdata
 inner join
     dim_zones as pickup_zone on fhv_tripdata.PUlocationID = pickup_zone.locationid
 inner join
