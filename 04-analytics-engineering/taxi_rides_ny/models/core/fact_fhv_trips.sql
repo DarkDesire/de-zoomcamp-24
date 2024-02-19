0281b1f8373be3effa1c3f559544089786f72147
Eldar Dragomir <oh.eldarkaa@gmail.com>
2024-02-19 17:06:20 +0100

    fhv (#7)
    * fhv
    
    * fhv
    
    * fhv2
    
    * fhv4
    
    ---------
    
    Co-authored-by: Eldar Dragomir <eldar.dragomir@gmail.com>

--- 04-analytics-engineering/taxi_rides_ny/models/core/fact_fhv_trips.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/core/fact_fhv_trips.sql
@@ -5,8 +5,20 @@ with
         select *, 'FHV' as service_type from {{ ref("stg_fhv_tripdata") }}
     ),
     dim_zones as (select * from {{ ref("dim_zones") }} where borough != 'Unknown')
-select fhv_tripdata.PUlocationID, fhv_tripdata.DOlocationID from fhv_tripdata
+select
+    fhv_tripdata.dispatching_base_num,
+    fhv_tripdata.pulocationid,
+    pickup_zone.borough as pickup_borough,
+    pickup_zone.zone as pickup_zone,
+    fhv_tripdata.dolocationid,
+    dropoff_zone.borough as dropoff_borough,
+    dropoff_zone.zone as dropoff_zone,
+    fhv_tripdata.pickup_datetime,
+    fhv_tripdata.dropoff_datetime,
+    fhv_tripdata.sr_flag,
+    fhv_tripdata.affiliated_base_number
+from fhv_tripdata
 inner join
-    dim_zones as pickup_zone on fhv_tripdata.PUlocationID = pickup_zone.locationid
+    dim_zones as pickup_zone on fhv_tripdata.pulocationid = pickup_zone.locationid
 inner join
-    dim_zones as dropoff_zone on fhv_tripdata.DOlocationID = dropoff_zone.locationid
+    dim_zones as dropoff_zone on fhv_tripdata.dolocationid = dropoff_zone.locationid
